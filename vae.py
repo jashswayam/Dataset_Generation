b536ff196190cb4a67590c3eb515446d95fdcdac
Jash Bhupendrakumar Tandel <86398574+jashswayam@users.noreply.github.com>
2025-10-24 17:35:31 +0530

    Update vae.py

--- vae.py
+++ vae.py
@@ -1,12 +1,20 @@
 import tensorflow as tf
-from tensorflow.keras import layers, Model
+from tensorflow import keras
+from tensorflow.keras import layers
 
-latent_dim = 2
-input_dim = 16  # example feature size
+# -----------------------------
+# Config
+# -----------------------------
+input_dim = 25
+latent_dim = 8
 
-# ----- Encoder -----
-encoder_inputs = tf.keras.Input(shape=(input_dim,))
-x = layers.Dense(32, activation="relu")(encoder_inputs)
+# -----------------------------
+# Encoder
+# -----------------------------
+inputs = keras.Input(shape=(input_dim,), name="encoder_input")
+x = layers.Dense(32, activation="relu")(inputs)
+x = layers.Dense(16, activation="relu")(x)
+
 z_mean = layers.Dense(latent_dim, name="z_mean")(x)
 z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
 
@@ -17,32 +25,73 @@ def sampling(args):
     return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
 z = layers.Lambda(sampling, name="z")([z_mean, z_log_var])
-encoder = Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+encoder = keras.Model(inputs, [z_mean, z_log_var, z], name="encoder")
+encoder.summary()
 
-# ----- Decoder -----
-latent_inputs = tf.keras.Input(shape=(latent_dim,))
-x = layers.Dense(32, activation="relu")(latent_inputs)
-decoder_outputs = layers.Dense(input_dim, activation="sigmoid")(x)
-decoder = Model(latent_inputs, decoder_outputs, name="decoder")
+# -----------------------------
+# Decoder
+# -----------------------------
+latent_inputs = keras.Input(shape=(latent_dim,), name="z_sampling")
+x = layers.Dense(16, activation="relu")(latent_inputs)
+x = layers.Dense(32, activation="relu")(x)
+outputs = layers.Dense(input_dim, activation="sigmoid")(x)
 
-# ----- VAE Model -----
-outputs = decoder(encoder(encoder_inputs)[2])
-vae = Model(encoder_inputs, outputs, name="vae")
+decoder = keras.Model(latent_inputs, outputs, name="decoder")
+decoder.summary()
 
-# ----- Custom Loss (Inside a Layer-Safe Context) -----
-# Use Keras backend or tf ops *inside add_loss*, not standalone tf.fn calls on symbolic tensors
-z_mean, z_log_var, z = encoder(encoder_inputs)
-reconstruction_loss = tf.reduce_mean(
-    tf.keras.losses.mse(encoder_inputs, outputs)
-)
+# -----------------------------
+# VAE Model
+# -----------------------------
+class VAE(keras.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
 
-kl_loss = -0.5 * tf.reduce_mean(
-    1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
-)
+    def compile(self, optimizer):
+        super(VAE, self).compile()
+        self.optimizer = optimizer
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(name="reconstruction_loss")
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
 
-vae.add_loss(reconstruction_loss + kl_loss)
-vae.add_metric(reconstruction_loss, name="reconstruction_loss")
-vae.add_metric(kl_loss, name="kl_loss")
+    @property
+    def metrics(self):
+        return [self.total_loss_tracker, self.reconstruction_loss_tracker, self.kl_loss_tracker]
 
-vae.compile(optimizer=tf.keras.optimizers.Adam())
-vae.summary()
+    def train_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            reconstruction_loss = tf.reduce_mean(
+                tf.reduce_sum(keras.losses.mse(data, reconstruction), axis=1)
+            )
+            kl_loss = -0.5 * tf.reduce_mean(
+                tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1)
+            )
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+
+# -----------------------------
+# Build and Train
+# -----------------------------
+vae = VAE(encoder, decoder)
+vae.compile(optimizer=keras.optimizers.Adam())
+
+# Dummy example data (replace with your dataset)
+import numpy as np
+X_train = np.random.rand(1000, input_dim).astype("float32")
+X_val = np.random.rand(200, input_dim).astype("float32")
+
+history = vae.fit(X_train, epochs=30, batch_size=32, validation_data=(X_val, None))
